3fdb0262afd4b623a1b16ab2641329b9fe87c24b
evalece <60049803+evalece@users.noreply.github.com>
2025-08-10 23:22:26 -0400

    useState summary & Adding interactive component

--- my-test/src/App.js
+++ my-test/src/App.js
@@ -1,34 +1,46 @@
 /*
-pass value with pros summary:
-Props = properties (data) passed from parent to child.
-
-Observation- 
-add value properties in functions using {}, this allows value passing with props via JSX 
-in format of < Square val='xxx' > assuming the square function returns a JSX 
-
-React rendering action: board passes value to square, square renders button
+Adding interactive component 
+useState => a special fucntion to rememebr interactions and value changes with it
+    - [x,xx] =>use xx('0') as a setValue function ipon change state value
 */
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
